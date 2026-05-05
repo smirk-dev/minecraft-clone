efb9cef39678254629e04e54746d34d93e7ea2df
smirk-dev <suryansh.mishra.sm6@gmail.com>
2026-05-05 22:04:29 +0530

    add constants for game configuration

---
+++ src/config/constants.ts
@@ -0,0 +1,26 @@
+export const CHUNK_X = 16;
+export const CHUNK_Z = 16;
+export const CHUNK_Y = 64;
+export const SEA_LEVEL = 24;
+
+export const PLAYER_HEIGHT = 1.8;
+export const PLAYER_EYE = 1.62;
+export const PLAYER_RADIUS = 0.3;
+
+export const GRAVITY = 28;
+export const WALK_SPEED = 4.3;
+export const SPRINT_SPEED = 6.5;
+export const JUMP_VELOCITY = 9;
+export const REACH = 6;
+
+export const RENDER_DISTANCE_DEFAULT = 4;
+export const CHUNK_GEN_BUDGET_MS = 6;
+export const MESH_BUDGET_MS = 4;
+
+export const DAY_LENGTH_SECONDS = 600;
+
+export const ATLAS_TILES = 16;
+export const ATLAS_TILE_PX = 16;
+export const ATLAS_PX = ATLAS_TILES * ATLAS_TILE_PX;
+
+export const STACK_SIZE = 64;
